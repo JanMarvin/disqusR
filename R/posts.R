1d987142e0538e69e0df4d3f6f34656f17c5c8a9
Marvin <jmg@edna>
2015-06-01 19:30:03 +0200

    getthreadid(), getposts() and documentation.
    Wrapper fuer threads() und posts() geben die Thread-Id und die posts als Text aus.

--- R/posts.R
+++ R/posts.R
@@ -1,7 +1,5 @@
 #' Disqus Posts API
-#' @param option string close, create, details, list, listHot, listPopular,
-#' listPosts, open, remove, restore, set, subscribe, unsubscribe, update, vote
-#' Options may differ in their set of Arguments.
+#' @param ... listTemplate option.
 #' @param category integer
 #' @param forum integer/string unique identifier of a forum
 #' @param thread integer or string if integer must be unique if string must
@@ -23,9 +21,10 @@
 #' threads("list", forum="politico", format="rss")
 #'
 #' # use a specific link ending with ".html" (may work if link was not changed)
-#' art <- "http://www.politico.com/magazine/story/2015/05/fox-news-liberals-118235.html"
-#' arts <- threads("list" , forum="politico", thread=paste0("link:", art))
-#' postslist <- posts("list", thread=arts[1,"id"])
+#' art <- "http://www.11freunde.de/liveticker/das-rueckspiel-der-relegation-im-liveticker"
+#' thrd <- threads("list", thread=art)
+#' pst <- posts("list", forum="11freunde", thread=thrd$id)
+#'
 #' # use a timestamp
 #' unixtime <- as.numeric(as.POSIXct("2015-05-26", format="%Y-%m-%d"))
 #' threads("list", forum="politico", since=unixtime)
@@ -38,6 +37,19 @@ posts <- function(...) {
                   flatten = TRUE)
 
   class(raw) <- c("dq_post")
+  return(raw)
+}
+
+#' getposts
+#'
+#' Wrapper for posts to extract and clean the returned posts.
+#' @param forum string or numeric
+#' @param thread string presumably collected by getthreadid.
+#' @param ... addition listTemplate options.
+#' @export
+getposts <- function(forum=forum, thread=thread, ...) {
+
+  raw <- posts(option="list", forum=forum, thread=thread, ...)
 
   pst <- raw$response
 
--- R/threads.R
+++ R/threads.R
@@ -8,6 +8,23 @@ threads <- function(...) {
   return(raw$response)
 }
 
+#' getthreadid
+#'
+#' Wrapper for threads() to fetch the id of a given forum.
+#'
+#' @param thrd numeric or string if is url this wrapper tries to fetch the id of
+#' the disqus thread in the link.
+#' @param ... additional listTemplate Options.
+#' @export
+getthreadid <- function(thrd=NULL, ...) {
+  if (!is.null(thrd))
+    raw <- threads(option="list", thread=thrd, ...)
+  else
+    stop("No thread choosen.")
+
+  return(raw$id)
+}
+
 #' @export
 threadsS4 <- function(...) {
   raw <- fromJSON(listTemplate(resource = "threads", ...))
